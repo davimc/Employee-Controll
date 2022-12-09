24906c0e39fea03634ab5af992270ac7f55b1887
davimc <davimatosc@hotmail.com>
2022-12-09 17:08:20 -0300

    first components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+import Header from './components/Header'
+import './App.css'
+import EmployeeCard from './components/EmployeeCard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <Header/>
+      <EmployeeCard/>
     </div>
   )
 }
---
+++ frontend/src/components/EmployeeCard/index.tsx
@@ -0,0 +1,97 @@
+import DatePicker from "react-datepicker"
+import { useState } from "react";
+import "react-datepicker/dist/react-datepicker.css"
+
+import './styles.css'
+
+function EmployeeCard() {
+
+    const min = new Date(new Date().setDate(new Date().getDate() - 365))
+    const max = new Date()
+
+    const [minDate, setMinDate] = useState(min)
+    const [maxDate, setMaxDate] = useState(max) 
+
+
+    return (
+        <div className="dsmeta-card">
+        <h2 className="dsmeta-sales-title">Funcionários</h2>
+        <div>
+        <div className="dsmeta-form-control-container">
+        <DatePicker
+            selected={minDate}
+            onChange={(date: Date) => {setMinDate(date)}}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+/>
+        </div>
+        <div className="dsmeta-form-control-container">
+        <DatePicker
+            selected={maxDate}
+            onChange={(date: Date) => {setMaxDate(date)}}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+/>
+        </div>
+        </div>
+
+        <div>
+        <table className="dsmeta-sales-table">
+            <thead>
+            <tr>
+                <th className="show992">ID</th>
+                <th className="show576">Data</th>
+                <th>Funcionário</th>
+                <th className="show992">CPF</th>
+                <th className="show992">Loja</th>
+                <th>Visualizar</th>
+            </tr>
+            </thead>
+            <tbody>
+            
+                <tr>
+                    <td className="show992">#322</td>
+                    <td className="show576">{new Date().toLocaleDateString()}</td>
+                    <td>Lucijane</td>
+                    <td className="show992">000.090.000-55</td>
+                    <td className="show992">Adminstrativo</td>
+                    <td>
+                    <div className="dsmeta-red-btn-container">
+                        {/* <NotificationButton saleId={sale.id} /> */}
+                    </div>
+                    </td>
+                </tr>  
+                <tr>
+                    <td className="show992">#322</td>
+                    <td className="show576">{new Date().toLocaleDateString()}</td>
+                    <td>Lucijane</td>
+                    <td className="show992">000.090.000-55</td>
+                    <td className="show992">Adminstrativo</td>
+                    <td>
+                    <div className="dsmeta-red-btn-container">
+                        {/* <NotificationButton saleId={sale.id} /> */}
+                    </div>
+                    </td>
+                </tr>  
+                <tr>
+                    <td className="show992">#322</td>
+                    <td className="show576">{new Date().toLocaleDateString()}</td>
+                    <td>Lucijane</td>
+                    <td className="show992">000.090.000-55</td>
+                    <td className="show992">Adminstrativo</td>
+                    <td>
+                    <div className="dsmeta-red-btn-container">
+                        {/* <NotificationButton saleId={sale.id} /> */}
+                    </div>
+                    </td>
+                </tr>  
+            </tbody>
+
+        </table>
+        </div>
+
+    </div>
+    )
+}
+
+export default EmployeeCard
