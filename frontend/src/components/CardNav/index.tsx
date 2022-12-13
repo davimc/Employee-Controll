0dc135f50eb22be9ff2b7221dec0c944921e0b99
davimc <davimatosc@hotmail.com>
2022-12-13 14:02:26 -0300

    trying fix

--- frontend/src/components/CardNav/index.tsx
+++ frontend/src/components/CardNav/index.tsx
@@ -1,62 +1,38 @@
-import { Listen } from 'ionicons/dist/types/stencil-public-runtime'
+import { useState } from 'react'
 import { HiOutlineBuildingStorefront, BsPerson, TbLicense, RiExchangeLine, FaRegHandshake } from 'react-icons/all'
+
 import './styles.css'
 
-const list = document.querySelectorAll('.active')
-function activeLink() {
-    list.forEach((item) =>
-        item.classList.remove('active'));
+const i = [<HiOutlineBuildingStorefront/>,'Lojas']
+const icons = new Map<string, [any,any,any]>()
     
-    this.classList.add('active')
-}
-list.forEach((item) => 
-    item.addEventListener('click', activeLink))
+icons.set('store', [<HiOutlineBuildingStorefront/>,'Lojas','#'])
+icons.set('employee', [<BsPerson/>, 'Funcionários','#'])
+icons.set('license', [<TbLicense/>, 'Licenças','#'])
+icons.set('exchange', [<RiExchangeLine/>, 'Trocas','#'])
+icons.set('request', [<FaRegHandshake/>, 'Pedidos','#'])
+
+const [classActive, setClassActive] = useState('store');
+
+
 function CardNav() {
     
         
     return(
         <div className="navigation">
-        <ul >
-            <li className="list " id='store'>
-                <a href="#">
-                    <span className="icon">
-                        <HiOutlineBuildingStorefront onClick={() =>activeLink()}/>
-                    </span>
-                    <span className="text">Stores</span>
-                </a>
-            </li>
-            <li className="list" id='employee'>
-                <a href="#">
-                    <span className="icon">
-                        <BsPerson/>
-                    </span>
-                    <span className="text">Employees</span>
-                </a>
-            </li>
-            <li className="list active">
-                <a href="#">
-                    <span className="icon">
-                        <TbLicense />
-                    </span>
-                    <span className="text">Licenses</span>
-                </a>
-            </li>
-            <li className="list">
-                <a href="#">
-                    <span className="icon">
-                        <RiExchangeLine />
-                    </span>
-                    <span className="text">Trocas</span>
-                </a>
-            </li>
-            <li className="list">
-                <a href="#">
-                    <span className="icon">
-                        <FaRegHandshake />
-                    </span>
-                    <span className="text">Pedidos</span>
-                </a>
-            </li>
+        <ul>
+            {Array.from(icons).map(([key,val]) => {
+                return(
+                <li className={classActive == key? 'list active': 'list'} id={key} key = {key}>
+                    <a href={val[2]} >
+                        <span className="icon" onClick={() =>setClassActive(key)}>
+                            {val[0]}
+                        </span>
+                        <span className="text">{val[1]}</span>
+                    </a>
+                </li>
+                )
+            })}
             <div className="indicator"></div>
         </ul>
     </div>
