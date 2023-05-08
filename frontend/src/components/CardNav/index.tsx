bf39b712ca1decd2b659bc2d00fd3f6307a2223b
davimc <davimatosc@hotmail.com>
2023-05-08 15:43:01 -0300

    testing

--- frontend/src/components/CardNav/index.tsx
+++ frontend/src/components/CardNav/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { HiOutlineBuildingStorefront, BsPerson, TbLicense, RiExchangeLine, FaRegHandshake } from 'react-icons/all'
 import { Link } from 'react-router-dom'
+import { BASE_URL } from '../../utils/request'
 
 import './styles.css'
 
@@ -23,7 +24,10 @@ function CardNav() {
         <ul>
             {Array.from(icons).map(([key,val]) => {
                 return(
-                <li className={classActive == key? 'list active': 'list'} id={key} key = {key} onClick={() => setClassActive(key)}>
+                <li className={classActive == key? 'list active': 'list'} id={key} key = {key} onClick={() => {
+                    {console.log(BASE_URL)}
+                    setClassActive(key)}
+                    }>
                     <Link to={val[2]} >
                         <span className="icon" >
                             {val[0]}
